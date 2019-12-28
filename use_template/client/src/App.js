ef8987b21bd6d58bd2d6cd4d421e762e95d08aee
unknown <ganesh.srambikal@gmail.com>
2019-12-28 16:06:27 +0530

    first

---
+++ use_template/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div className="App">
+      <>
+    </div>
+  );
+}
+
+export default App;
